3ae46da0049486e8210ec89640fd351a2965f2a7
Sultan El Jabali <sultan.eljabali@gmail.com>
2022-03-09 17:33:16 -0500

    Update

--- capstone/TenmoServer/Controllers/AccountController.cs
+++ capstone/TenmoServer/Controllers/AccountController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TenmoServer.Models;
 using TenmoServer.DAO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
-
 namespace TenmoServer.Controllers
 {
-    public class AccountController
+    [Route("account")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AccountController : ControllerBase 
     {
+        private readonly IAccountSqlDao accountSqlDao;
+
+        public AccountController(IAccountSqlDao accountSqlDao)
+        {
+            this.accountSqlDao = accountSqlDao;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Account> GetAccounts(int id)
+        {
+            Account account = accountSqlDao.GetAccount(id);
+            if (account != null)
+            {
+                return Ok(account);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
     }
 }
