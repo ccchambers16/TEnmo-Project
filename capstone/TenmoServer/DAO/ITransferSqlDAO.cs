619b787bb1b5af3ff7ea163f38aaeb5a634e5e3b
Sultan El Jabali <sultan.eljabali@gmail.com>
2022-03-10 11:07:31 -0500

    Update

--- capstone/TenmoServer/DAO/ITransferSqlDAO.cs
+++ capstone/TenmoServer/DAO/ITransferSqlDAO.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TenmoServer.Models;
 
 namespace TenmoServer.DAO
 {
     public class ITransferSqlDAO
     {
+        Transfer GetTransfer(int transferId);
+
+        List<Transfer> GetAllTransfers(int userId);
+        Transfer SendTransfer(int userId, int TouserId, double TransferAmount);
+
     }
 }
--- capstone/TenmoServer/Models/Transfer.cs
+++ capstone/TenmoServer/Models/Transfer.cs
@@ -2,31 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TenmoServer.Models
 {
     public class Transfer
     {
+        [Range(3000, double.PositiveInfinity, ErrorMessage = "The field `TransferId` must not be negative.")]
         int TransferId { get; set; }
+        [Range(1000, double.PositiveInfinity, ErrorMessage = "The field 'UserId' must be a valid id.")]
 
         int SenderUserId { get; set; }
-
+        [Range(1,2, ErrorMessage = "The field `Balance` must not be negative.")]
         int TransferTypeId { get; set; }
 
         int TransferStatusId { get; set; }
 
+        [Range(2000, double.PositiveInfinity, ErrorMessage = "The field 'AccountId' must be a valid id.")]
 
         int FromAccountId { get; set; }
-
+        [Range(0.00, double.PositiveInfinity, ErrorMessage = "The field `FromBalance` must not be negative.")]
         decimal FromBalance { get; set; }
-
+        [Range(0.00, double.PositiveInfinity, ErrorMessage = "The field 'TransferAmount' must not be negative.")]
         decimal TransferAmount { get; set; }
 
-        
+        [Range(1000, double.PositiveInfinity, ErrorMessage = "The field 'ToUserId' must be a valid id.")]
+
         int ToUserId { get; set; }
+        [Range(2000, double.PositiveInfinity, ErrorMessage = "The field 'ToAccountId' must be a valid id.")]
 
         int ToAccountId { get; set; }
-
+        [Range(0.00, double.PositiveInfinity, ErrorMessage = "The field `ToBalance` must not be negative.")]
         decimal ToBalance { get; set; }
 
 
